4018709618d7ded03136694ff5a543133bafb1e8
ya_razum <jaroro900@gmail.com>
2025-02-24 15:29:49 +0100

    comment

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,1 +1,23 @@
-git
+max_x = 500
+max_y = 500
+
+x = 10
+y = 10
+
+dx = 1
+dy = 1
+
+def draw_circle(x,y):
+    print((x, y))
+
+while True:
+    x += dx
+    y += dy
+
+    if x > max_x or x <= 0:
+        dx = -dx
+
+    if y > max_y or y <= 0:
+        dy = -dy
+
+    draw_circle(x, y)
