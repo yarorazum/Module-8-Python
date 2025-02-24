d23169b84af5e097b745b0f7e222f363ae19151f
ya_razum <jaroro900@gmail.com>
2025-02-24 16:18:48 +0100

    comment 3

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,4 +1,6 @@
-max_x = 500
+
+#Create imaginary bouncing ball "game": you should set some screen size, and make the circle changing coordinates bouncing from screen edge.
+"""max_x = 500
 max_y = 500
 
 x = 10
@@ -18,4 +20,48 @@ while True:
     if y > max_y or y <= 0:
         dy = -dy
 
-    draw_circle(x, y)
+    draw_circle(x, y)"""
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Bouncing Ball")
+
+clock = pygame.time.Clock()
+
+running = True
+
+max_x = 500
+max_y = 500
+x = 10
+y = 10
+dx = 5
+dy = 5
+radius = 20
+ball_color = (255, 255, 255)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    x += dx
+    y += dy
+
+    if x >= max_x or x <= 0:
+        dx = -dx
+    if y >= max_y or y <= 0:
+        dy = -dy
+
+    screen.fill("purple")
+
+    pygame.draw.circle(screen, ball_color, (x, y), radius)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
+
